ec9ee28d3fa4f4d9b012b1165ae92eb5966d6902
eliel <elielmeschss@gmail.com>
2022-07-27 19:38:54 -0300

    alterações package

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
